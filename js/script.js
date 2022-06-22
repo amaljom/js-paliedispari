6d6df4045c7c659cd2fae6f68db9e467f10a18ef
amaljom <amaljom99@gmail.com>
2022-06-22 16:56:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// * FUNZIONE PER CAPIRE SE SI TRATTA DI UNA PALINDROMO O MENO
+function analisiPalindromo(word){
+    
+    let i=0;
+    while(i<word.leght/2){
+        if( word[i] == word.leght-1-i ){
+            
+        }
+    }
+    
+} 
+
+
+
+// ? CHIEDIAMO AL UTENTE DI INSERIRE UNA PAROLA IN MODO DA POTERCI POI EFFETTUARE LE ANALISI
+const userWord= prompt('inserisci una parola!');
+
+
+
