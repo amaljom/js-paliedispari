c014b0b41c70c2a1dea7b1fe1f51168b2d7b094e
amaljom <amaljom99@gmail.com>
2022-06-22 17:45:19 +0200

    tolto il primo esericizio dai commenti

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 // * FUNZIONE PER CAPIRE SE SI TRATTA DI UNA PALINDROMO O MENO
-// function analisiPalindromo(word){
-//     let condition=false;
-//     let lunghezza=word.length
-//     let i=0;
-//     while(i<word.length/2){
-//         if( word[i] === word[lunghezza - i -1] ){
-//             i++;
-//             condition=true;
-//         }
-//         else{
-//             i=word.length/2;
-//             condition=false;
-//         }
-//     }
-//     return condition;
-// } 
+function analisiPalindromo(word){
+    let condition=false;
+    let lunghezza=word.length
+    let i=0;
+    while(i<word.length/2){
+        if( word[i] === word[lunghezza - i -1] ){
+            i++;
+            condition=true;
+        }
+        else{
+            i=word.length/2;
+            condition=false;
+        }
+    }
+    return condition;
+} 
 // ! HO CREATO UN CICLO CHE CONFRONTI LE DUE ESTREMITA' DELLA PAROLA
 // ! IN QUESTO MODO LA FUNZIONA SI MUOVE DA SOLO DALLE DUE ESTREMITA' VERSO IL CENTRO
 
 
 // ? CHIEDIAMO AL UTENTE DI INSERIRE UNA PAROLA IN MODO DA POTERCI POI EFFETTUARE LE ANALISI
-// const userWord= prompt('inserisci una parola!');
+const userWord= prompt('inserisci una parola!');
 
-// let esitoTest= analisiPalindromo(userWord);
+let esitoTest= analisiPalindromo(userWord);
 
-// if (esitoTest){
-//     console.log('è una parola palindroma');
-// }
-// else{
-//     console.log('ritenta!');
-// }
+if (esitoTest){
+    console.log('è una parola palindroma');
+}
+else{
+    console.log('ritenta!');
+}
 
 // * CREIAMO UNA FUNZIONA CHE GENERI AUTOMATICAMENTE UN NUMERO PER IL CALCOLATORE
 
